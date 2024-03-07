035084951d33948c1cbd13a574e022b7b9af186f
Nilesh <nileshk@whiteorangesoftware.com>
2024-03-07 10:19:37 +0530

    upload version 0.0.2 and update readme.md and licence

---
+++ lib/flutter_timezone_name.dart
@@ -0,0 +1,8 @@
+library flutter_timezone_name;
+export 'src/flutter_timezone_name.dart';
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
